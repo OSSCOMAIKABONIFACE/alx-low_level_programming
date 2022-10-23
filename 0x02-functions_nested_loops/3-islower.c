2629f154c2206b923d602db10e118803c002ac3d
Your Name <oscomaikabonif........com>
2022-10-23 13:08:15 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*_islower - check that a char is a lower case
+*@c: is the char to be checked
+*Return: 1 if char us lower case,
+*otherwuse 0.
+*int_islower (int c)
+*{
+*if (c >='a' && c <=('2')
+*else
+*return (0);
